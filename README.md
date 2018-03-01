fc0ef9f4c52afb5fd70462be6f7050f0344561fa
Andreas Tornes <andreas.tornes@bitvis.no>
2018-03-01 13:30:32 +0100

    BV_KREM-195: Added default setup.py

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Clone directly into _\<your\ krem\ project\>/library/plugins_
 
+Copy the file _krem\_plugins/setup.py_ into _\<your\ krem\ project\>/library_ and uncomment the plugins you want to use
+
 See _\<krem\ installation\>/docs/KREM\_USER\_MANUAL.md_ for guide to setup plugins in your project.
---
+++ setup.py
@@ -0,0 +1,40 @@
+from krempack.core import plugin
+
+'''
+Use following import if krem_plugins repo is placed in library,
+or add other plugins in the same way
+'''
+from library.plugins.krem_plugins.print_task_results.print_task_results import PluginPrintTaskResults
+from library.plugins.krem_plugins.debug_terminal.debug_terminal import PluginDebugTerminal
+from library.plugins.krem_plugins.help_docs.help_docs import PluginHelpDocs
+from library.plugins.krem_plugins.task_lister.task_lister import PluginTaskLister
+from library.plugins.krem_plugins.task_log_printer.task_log_printer import PluginTaskLogPrinter
+from library.plugins.krem_plugins.time_task_function.time_task_function import PluginTimeTaskFunction
+
+
+def setup_plugins(plugin_handler):
+    # Register runtime plugins here
+    '''
+    Uncomment the following to enable some of the plugins from krem_plugins,
+    or add other plugins in the same way
+    '''
+    #plugin_handler.register_plugin(PluginPrintTaskResults)
+    #plugin_handler.register_plugin(PluginDebugTerminal)
+    #plugin_handler.register_plugin(PluginTimeTaskFunction)
+
+
+
+    pass
+
+def setup_cli_plugins(plugin_handler):
+    # Register CLI plugins here
+    '''
+    Uncomment the following to enable some of the plugins from krem_plugins,
+    or add other plugins in the same way
+    '''
+    #plugin_handler.register_plugin(PluginHelpDocs)
+    #plugin_handler.register_plugin(PluginTaskLister)
+    #plugin_handler.register_plugin(TaskLogPrinter)
+
+    pass
+
