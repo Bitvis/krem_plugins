a40942ee87e888eabb6274f5533e4692407b5728
Michal Koziel <michal.koziel@bitvis.no>
2018-03-15 10:34:44 +0100

    BV_KREM-237 fixing: Terminal's issue/info is printing when krem is excecuted

--- task_tasks_log_terminal/task_tasks_log_terminal.py
+++ task_tasks_log_terminal/task_tasks_log_terminal.py
@@ -3,6 +3,7 @@ import sys
 from krempack.core import plugin
 from krempack.common import kremtree
 from subprocess import Popen, PIPE
+import subprocess
 from multiprocessing import Process
 from time import sleep
 
@@ -16,7 +17,7 @@ use_default_terminal = False
 
 
 class PluginTaskTasksLogTerminal(plugin.Plugin):
-    name = "debug-terminal"
+    name = "Task tasks log terminal"
     process = None
 
     def __init__(self):
@@ -50,7 +51,7 @@ class PluginTaskTasksLogTerminal(plugin.Plugin):
         term_cmd = None
         
         if terminal == "gnome-terminal":
-            term_cmd = "gnome-terminal -e "
+            term_cmd = "gnome-termina l -e "
         elif terminal ==  "xterm":
             term_cmd = "xterm -hold -e "
         elif terminal == "uxterm":
@@ -58,7 +59,7 @@ class PluginTaskTasksLogTerminal(plugin.Plugin):
         elif terminal == "lxterm":
             term_cmd = "lxterm -e "
         else:
-            self.log.write('Failed to get terminal name. Ensure you are running a terminal supported by the debug-terminal plugin', 'error')
+            self.log.write("Failed to get terminal name. Ensure you are running a terminal supported by the plugin '{}'".format(PluginTaskTasksLogTerminal.name), 'error')
         return term_cmd
 
     def tracer(self, log_path, job):
@@ -73,19 +74,21 @@ class PluginTaskTasksLogTerminal(plugin.Plugin):
                 if success:
                     break
 
-        if not success:
-            self.log.write("Failed to launch plugin 'debug-terminal'", "error")
+        if not success:            
+            self.log.write("Failed to launch plugin '{}'".format(PluginTaskTasksLogTerminal.name), "error")
 
     def try_to_run(self, terminal, log_path, job):
         cmd = str(self.get_terminal_command(terminal, job)) + '"tail -f ' + log_path + '"'
-        success = True
+        success = False
 
         try:
-            if os.system(cmd):
-                success= False
-        except Exception as e:
+            subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+            # the above command execution returned 0, other wise we get an exception below
+            success = True
+        except subprocess.CalledProcessError as e:
             success = False
- 
+        except Exception:
+            success = False
         return success
 
     def job_start(self, job):
@@ -98,15 +101,15 @@ class PluginTaskTasksLogTerminal(plugin.Plugin):
         try:
             PluginTaskTasksLogTerminal.process = Process(target=self.tracer, args=(task_log_path,job,)) 
             PluginTaskTasksLogTerminal.process.start()
-            self.log.write('Debug terminal started', 'debug')
+            self.log.write("Plugin '{}' started".format(PluginTaskTasksLogTerminal.name), 'debug')
             sleep(0.5)
         except Exception as e:
-            self.log.write('Failed to start debug terminal. Exception raised: ' + str(e), 'error')
+            self.log.write("Failed to start plugin '{}' plugin. Exception raised: ".format(PluginTaskTasksLogTerminal.name) + str(e), 'error')
 
     def job_end(self, job):
         if PluginTaskTasksLogTerminal.process.is_alive():
             try:
                 PluginTaskTasksLogTerminal.process.terminate()
-                self.log.write('Debug terminal stopped', 'debug')
+                self.log.write("Plugin '{}' stopped".format(PluginTaskTasksLogTerminal.name), 'debug')
             except Exception as e:
-                self.log.write('Failed to stop debug terminal. Exception raised: ' + str(e), 'error')
+                self.log.write("Failed to stop plugin '{}'. Exception raised: ".format(PluginTaskTasksLogTerminal.name) + str(e), 'error')
